9d64d10b5c7f69f1f847df50f6da3ff730d1923b
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2022-12-11 22:19:43 +0100

    Initial commit

---
+++ src/main/java/com/krabelard/safeapp/SafeAppApplication.java
@@ -0,0 +1,13 @@
+package com.krabelard.safeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krabelard/safeapp/SafeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krabelard.safeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
