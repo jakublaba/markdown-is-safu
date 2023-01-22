c19292e3d35ac9b17b64a3091e38f28d20b26eab
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2023-01-22 12:30:33 +0100

    Configure https for frontend

--- frontend/src/types/axios-types.ts
+++ frontend/src/types/axios-types.ts
@@ -3,24 +3,24 @@ export const AUTH_API_URL = "http://localhost:8080/api/auth";
 export const NOTE_API_URL = "http://localhost:8080/api/notes";
 
 export interface IRegisterRequest {
-    username: string,
-    email: string,
-    password: string
+    username: string;
+    email: string;
+    password: string;
 }
 
 export interface ILoginResponse {
-    username: string,
-    email: string,
-    jwt: string
+    username: string;
+    email: string;
+    jwt: string;
 }
 
 export interface IUser {
-    username: string,
-    email: string
+    username: string;
+    email: string;
 }
 
 export interface INote {
-    uuid: string,
-    name: string,
-    owner: IUser
+    uuid: string;
+    name: string;
+    owner: IUser;
 }
